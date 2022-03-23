eaaea7e8c76e727f96b3d8756b038ef2d0cb14f6
bbun0639 <pathomphob.sun@gmail.com>
2022-03-23 18:33:26 +0700

    feat: merge some file that still conflict

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-=======
-import React from "react";
->>>>>>> 735e4cb (feat: login form)
 import { FieldRenderProps } from "react-final-form";
 
 type Props = FieldRenderProps<string, any>;
 
-<<<<<<< HEAD
 const TextInput = ({ input, meta, ...rest }: Props) => (
-=======
-const TextInput: React.FC<Props> = ({ input, meta, ...rest }: Props) => (
->>>>>>> 735e4cb (feat: login form)
   <input type="text" {...input} {...rest} />
 );
 
